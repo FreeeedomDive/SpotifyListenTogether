753cd27db2a15774ebb526382b86e3382c1530d1
Igor Chernyshev <sonic96.ic@gmail.com>
2023-12-14 22:51:07 +0500

    another way to correctly escape symbols

--- Core/Extensions/FormattingExtensions.cs
+++ Core/Extensions/FormattingExtensions.cs
@@ -6,25 +6,20 @@ public static class FormattingExtensions
 {
     public static string ToFormattedString(this FullTrack track)
     {
-        return $"[{track.Artists.First().Name} - {track.Name}]({track.ExternalUrls["spotify"]})".EscapeTelegramReservedSymbols();
+        return $"[{(track.Artists.First().Name + " - " + track.Name).Escape()}]({track.ExternalUrls["spotify"]})";
     }
 
     public static string ToFormattedString(this FullAlbum album)
     {
-        return $"[{album.Artists.First().Name} - {album.Name}]({album.ExternalUrls["spotify"]})".EscapeTelegramReservedSymbols();
-    }
-
-    public static string ToFormattedString(this SimpleArtist artist)
-    {
-        return $"[{artist.Name}]({artist.ExternalUrls["spotify"]})".EscapeTelegramReservedSymbols();
+        return $"[{(album.Artists.First().Name + " - " + album.Name).Escape()}]({album.ExternalUrls["spotify"]})";
     }
 
     public static string ToFormattedString(this FullPlaylist playlist)
     {
-        return $"[{playlist.Name}]({playlist.ExternalUrls!["spotify"]})".EscapeTelegramReservedSymbols();
+        return $"[{playlist.Name!.Escape()}]({playlist.ExternalUrls!["spotify"]})";
     }
 
-    private static string EscapeTelegramReservedSymbols(this string str)
+    private static string Escape(this string str)
     {
         return str.Replace("-", "\\-")
                   .Replace("(", "\\(")
