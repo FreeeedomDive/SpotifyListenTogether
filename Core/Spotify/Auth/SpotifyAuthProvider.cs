defd779f9ac596527291ec887759fa7cc251d4d6
Igor Chernyshev <sonic96.ic@gmail.com>
2024-02-10 20:20:05 +0500

    let's see...

--- Core/Spotify/Auth/SpotifyAuthProvider.cs
+++ Core/Spotify/Auth/SpotifyAuthProvider.cs
@@ -53,6 +53,7 @@ public class SpotifyAuthProvider : ISpotifyAuthProvider
     {
         await server.Stop();
 
+        Console.WriteLine(response.Code);
         var tokenResponse = await new OAuthClient().RequestToken(
             new AuthorizationCodeTokenRequest(
                 spotifySettings.Value.ClientId, spotifySettings.Value.ClientSecret, response.Code, new Uri(spotifySettings.Value.RedirectUri)
