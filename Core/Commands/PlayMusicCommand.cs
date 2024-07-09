ea16658a14bc5e1836e3fb7a908a31ad22ec4a96
Igor Chernyshev <sonic96.ic@gmail.com>
2024-07-09 20:06:10 +0500

    disable shuffle turn off because of broken api response

--- Core/Commands/PlayMusicCommand.cs
+++ Core/Commands/PlayMusicCommand.cs
@@ -126,7 +126,7 @@ public class PlayMusicCommand
         var result = await this.ApplyToAllParticipants(
             async (client, participant) =>
             {
-                await client.Player.SetShuffle(new PlayerShuffleRequest(false));
+                // await client.Player.SetShuffle(new PlayerShuffleRequest(false)); // TODO: SpotifyApi returns unexpected response, but library expects boolean value
                 await client.Player.ResumePlayback(
                     new PlayerResumePlaybackRequest
                     {
@@ -153,7 +153,7 @@ public class PlayMusicCommand
         var result = await this.ApplyToAllParticipants(
             async (client, participant) =>
             {
-                await client.Player.SetShuffle(new PlayerShuffleRequest(false));
+                // await client.Player.SetShuffle(new PlayerShuffleRequest(false)); // TODO: SpotifyApi returns unexpected response, but library expects boolean value
                 await client.Player.ResumePlayback(
                     new PlayerResumePlaybackRequest
                     {
