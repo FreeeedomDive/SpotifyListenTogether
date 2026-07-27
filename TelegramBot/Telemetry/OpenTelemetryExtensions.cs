94aecce84ca5e40bb15cdd98b7bed29cbdec22f5
Igor Chernyshev <sonic96.ic@gmail.com>
2026-07-28 02:04:55 +0500

    more buckets

--- TelegramBot/Telemetry/OpenTelemetryExtensions.cs
+++ TelegramBot/Telemetry/OpenTelemetryExtensions.cs
@@ -136,7 +136,10 @@ public static class OpenTelemetryExtensions
     private const string GrpcSourcePrefix = "Grpc";
 
     private static readonly double[] DurationBuckets =
-        [0.01, 0.025, 0.05, 0.075, 0.1, 0.25, 0.5, 0.75, 1, 2.5, 5, 7.5, 10];
+    [
+        0.01, 0.025, 0.05, 0.075, 0.1, 0.15, 0.2, 0.25, 0.35, 0.5,
+        0.75, 1, 1.25, 1.5, 2, 2.5, 5, 7.5, 10,
+    ];
 
     private static readonly double[] ParticipantsBuckets =
         [1, 2, 3, 4, 5, 6, 8, 10, 15, 20];
